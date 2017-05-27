3c68824d99657a72ff92922b271c8aaa63c6720f
Monique St. Laurent <moinstla@gmail.com>
2017-05-26 19:03:43 -0700

    add images and bios

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,13 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-var displayDoctor = function(medicalIssue, firstName, lastName) {
-    $('.showDoctors').append("<li>" + firstName + " " + lastName + "</li>");
+var displayDoctor = function(medicalIssue, img, firstName, lastName, title, bio) {
+    $('.showDoctors').append('<li><img src="' + img + '"><br><h4>'+ firstName + " " + lastName + "," + title + "</h4>" + bio + "</li><br><hr>");
 };
 
 $(document).ready(function() {
   var doctorObject = new Doctor();
   $('#submit-issue').click(function() {
-    $( ".showDoctors" ).empty();
+    $(".showDoctors" ).empty();
     var medicalIssue = $('#issue').val();
     $('#issue').val("");
     doctorObject.getDoctor(medicalIssue, displayDoctor);
--- js/doctor.js
+++ js/doctor.js
@@ -7,10 +7,10 @@ Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
       for (var i = 0; i <= response.data.length; i++) {
 
-            displayDoctor(medicalIssue, response.data[i].profile.first_name, response.data[i].profile.last_name);
+            displayDoctor(medicalIssue, response.data[i].profile.image_url, response.data[i].profile.first_name, response.data[i].profile.last_name, response.data[i].profile.title, response.data[i].profile.bio);
           }
         }).fail(function(error) {
-          $('.showDoctors').text(error.responseJSON.message);
+          $('#showDoctors').text(error.responseJSON.message);
         });
       };
 
